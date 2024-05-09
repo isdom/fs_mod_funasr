31c90c11c5ff56df8cb2798965a6095f66559f49
maming <ming.ma@yulore.com>
2024-05-09 17:53:24 +0800

    move global var to fun_asr_global_t

--- mod_funasr.cpp
+++ mod_funasr.cpp
@@ -823,7 +823,7 @@ static void destroy_fun_asr(fun_asr_context_t *pvt) {
         }
     }
     switch_mutex_destroy(pvt->mutex);
-    if (g_debug) {
+    if (fun_asr_globals->_debug) {
         switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_NOTICE,
                           "destroy_fun_asr: switch_mutex_destroy -> on channel: %s\n",
                           switch_channel_get_name(channel));
